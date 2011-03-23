4b2e7cbc4829fb184510b709d0ea9a5a273a8cbc
Joël Perras <joel.perras@gmail.com>
2011-03-23 10:42:13 -0400

    Adding start of deceptabot.

---
+++ vendor/google/google.js
@@ -0,0 +1,30 @@
+/**
+ * Shamelessly taken from https://github.com/gf3/protobot
+ */
+
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+function Google() {
+  this.search = function(query, hollaback) {
+    exec("curl -XGET 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + escape(query) + "'", function (err, stdout, stderr) {
+      var results = JSON.parse(stdout)['responseData']['results'];
+      results.forEach(function(x) {
+        x.titleNoFormatting = x.titleNoFormatting.replace(/&#([^\s]*);/g, function(m1, m2) {
+          return String.fromCharCode(m2);
+        }).replace(/&(nbsp|amp|quot|lt|gt);/g, function(m1, m2) {
+          return { 'nbsp': ' ', 'amp': '&', 'quot': '"', 'lt': '<', 'gt': '>' }[m2];
+        });
+        return x;
+      });
+      hollaback.call(this, results);
+    });
+  };
+}
+module.exports = Google;
+
+//g = new Google();
+//g.search('nerderati', function(result) {
+	//sys.puts(result.length);
+	//sys.puts(sys.inspect(result));
+//});
