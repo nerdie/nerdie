ba945e39638cbdfebb2f3b1d47a8ee0f2d76f4e5
Joël Perras <joel.perras@gmail.com>
2011-05-22 00:16:48 -0400

    Adding 'convert' plugin for currency conversions.

---
+++ plugins/convert.js
@@ -0,0 +1,52 @@
+var request = require('request');
+
+var NerdieInterface = require('../nerdie_interface.js');
+module.exports = Plugin;
+
+function Plugin(parentNerdie) {
+	this.pluginInterface = new NerdieInterface(parentNerdie, this);
+}
+Plugin.prototype.init = function () {
+	this.pluginInterface.registerPattern(
+		this.pluginInterface.anchoredPattern('convert', true),
+		this.getConversion
+	);
+};
+
+Plugin.prototype.getConversion = function(msg) {
+	var toConvert = msg.match_data[2].split('to');
+
+	if (toConvert.length !== 2) {
+		toConvert = msg.match_data[2].split('in');
+
+		if (toConvert.length !== 2) {
+			msg.say('Invalid conversion request. Example usage: 1 USD to GBP');
+			return;
+		}
+	}
+
+	var data = {'from': toConvert[0], 'to': toConvert[1]};
+	var uri = 'http://google.com/ig/calculator?q=' + encodeURIComponent(data['from']) + '=?' + encodeURIComponent(data['to']);
+
+	request({uri: uri}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			try {
+				body = JSON.parse(body.replace(/(\w+):/g, "\"$1\":"));
+			} catch (except) {
+				msg.say("Argh. Could not parse response from " + uri + " .");
+				return;
+			}
+
+			if (body.error !== "") {
+				msg.say('Whoops! I had some trouble converting that. Sorry :-/.');
+				return;
+			}
+			msg.say(msg.user + ": " + body.lhs + " is " + body.rhs);
+		} else {
+			msg.say('Whoops! I had some trouble converting that. Sorry :-/.');
+			return;
+		}
+	});
+};
+
+
