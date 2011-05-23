16414cbf5d142c2831a9821cfee8fcc8f758e464
Sean Coates <sean@seancoates.com>
2011-05-22 23:32:02 -0400

    rename sandbox plugin object

--- plugins/sandbox.js
+++ plugins/sandbox.js
@@ -2,19 +2,19 @@ var sandbox = require('sandbox');
 
 var NerdieInterface = require('../nerdie_interface.js');
 
-module.exports = Plugin;
+module.exports = Sandbox;
 
-function Plugin(parentNerdie) {
+function Sandbox(parentNerdie) {
 	this.pluginInterface = new NerdieInterface(parentNerdie, this);
 }
-Plugin.prototype.init = function () {
+Sandbox.prototype.init = function () {
 	this.pluginInterface.registerPattern(
 		this.pluginInterface.anchoredPattern('js', true),
 		this.getSandbox
 	);
 };
 
-Plugin.prototype.getSandbox = function(msg) {
+Sandbox.prototype.getSandbox = function(msg) {
 	s = new sandbox();
 	s.run(msg.match_data[2], function(output) {
 		msg.say(msg.user + ": Result is => " + output.result);
