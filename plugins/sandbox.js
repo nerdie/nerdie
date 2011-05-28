fa7c03ed811d69d33ca19259058a435dd6526a25
Sean Coates <sean@seancoates.com>
2011-05-28 14:46:54 -0400

    add codepad to sandbox

--- plugins/sandbox.js
+++ plugins/sandbox.js
@@ -1,4 +1,5 @@
-var sandbox = require('sandbox');
+var sandbox = require('sandbox')
+  , codepad = require('codepad');
 
 var NerdieInterface = require('../nerdie_interface.js');
 
@@ -10,13 +11,52 @@ function Sandbox(parentNerdie) {
 Sandbox.prototype.init = function () {
 	this.pluginInterface.registerPattern(
 		this.pluginInterface.anchoredPattern('js', true),
-		this.getSandbox
+		this.getJsSandbox
+	);
+
+	this.pluginInterface.registerPattern(
+		this.pluginInterface.anchoredPattern('eval', true),
+		this.getEval
 	);
 };
 
-Sandbox.prototype.getSandbox = function(msg) {
+Sandbox.prototype.getJsSandbox = function(msg) {
 	s = new sandbox();
 	s.run(msg.match_data[2], function(output) {
 		msg.say(msg.user + ": Result is => " + output.result);
 	});
 };
+
+Sandbox.prototype.getEval = function(msg) {
+	var txt = msg.match_data[2];
+	var evalLang = txt.substr(0, txt.indexOf(' '));
+	var evalCode = txt.substr(txt.indexOf(' ') + 1);
+	if (!evalLang || !evalCode) {
+		// no language or code supplied
+		return
+	}
+
+	try {
+		codepad.eval(evalLang, evalCode, function (err, data) {
+			if (err) {
+				msg.say(msg.user + ": codepad error.");
+				return;
+			}
+			var out = data.output.trim().split("\n");
+			msg.say(msg.user + ": " + out[0])
+			var txt = msg.user + ": ";
+			if (out.length > 1) {
+				txt += "+" + (out.length - 1) + " more line";
+				if (out.length > 2) {
+					txt += "s";
+				}
+				txt += " ";
+				msg.say(txt + "-> " + data.url);
+			}
+		}, true, 'nerdie');
+	} catch (e) {
+		msg.say(msg.user + ": " + e);
+	}
+
+};
+
