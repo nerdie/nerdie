d81fd936695729a666da04e7f3c077032163f692
Sean Coates <sean@seancoates.com>
2011-07-17 22:35:25 -0400

    add support for plugins/user/

--- server.js
+++ server.js
@@ -25,32 +25,73 @@ Nerdie.prototype.bot = jerk(function(j){
 	  , loadedPlugins = {}
 	  , name = null;
 
+	require.paths.push(__dirname);
+	var loadPlugin = function (prefix, filename) {
+		name = prefix + filename.split('.').slice(0, -1).join('.');
+		var pluginLoader = require(name);
+		plugin = new pluginLoader(nerdie);
+
+		if ('object' == typeof plugin.pluginInterface) {
+			plugin.pluginInterface.addListener('registerPattern', function (pattern, callback) {
+				console.log('Registered pattern: ' + pattern);
+				j.watch_for(pattern, callback);
+			});
+		}
+		loadedPlugins[name] = plugin;
+		console.log('Loaded ' + name + ' plugin.');
+	}
+
+	var loadedCore = false, loadedUser = false;
+	var doInit = function () {
+		if (loadedCore && loadedUser) {
+			nerdie.emit('init', config, loadedPlugins);
+		}
+	};
+
+	// core plugins:
 	fs.readdir('plugins', function(err, files) {
 		if (err) {
 			throw err;
 		}
 		files.forEach(function (filename) {
-			var pluginLoader = null;
-			if (!fs.statSync("./plugins/" + filename).isFile()) {
-				// not a file
-				return;
-			}
-			if (filename.match(/.+\.js$/i)) {
-				name = "./plugins/" + filename.split('.').slice(0, -1).join('.');
-				pluginLoader = require(name);
-				plugin = new pluginLoader(nerdie);
-
-				if ('object' == typeof plugin.pluginInterface) {
-					plugin.pluginInterface.addListener('registerPattern', function (pattern, callback) {
-						console.log('Registered pattern: ' + pattern);
-						j.watch_for(pattern, callback);
-					});
-				}
-				loadedPlugins[name] = plugin;
-				console.log('Loaded ' + name + ' plugin.');
+			var prefix = __dirname + '/plugins/';
+			if (fs.statSync(prefix + filename).isFile()
+				&& filename.match(/.+\.js$/i)) {
+				loadPlugin(prefix, filename);
 			}
 		});
-		nerdie.emit('init', config, loadedPlugins);
+		loadedCore = true;
+		doInit();
+	});
+
+	fs.readdir('plugins/user', function(err, files) {
+		if (err) {
+			throw err;
+		}
+		files.forEach(function (filename) {
+			var prefix = __dirname + '/plugins/user/'
+			var stat = fs.statSync(prefix + filename);
+			if (stat.isFile() && filename.match(/.+\.js$/i)) {
+				loadPlugin(__dirname + '/plugins/user/', filename);
+			} else if (stat.isDirectory()) {
+				// plugins/user/{pluginname}/
+				prefix = __dirname + '/plugins/user/' + filename + '/';
+				// check pluginname/index.js
+				try {
+					fs.statSync(prefix + 'index.js');
+					loadPlugin(prefix, 'index.js');
+					return; // forEach iteration
+				} catch (e) {}
+				// check pluginname/pluginname.js
+				try {
+					fs.statSync(prefix + filename + '.js');
+					loadPlugin(prefix, filename + '.js');
+					return; // forEach iteration
+				} catch (e) {}
+			}
+		});
+		loadedUser = true;
+		doInit();
 	});
 
 }).connect(config);
