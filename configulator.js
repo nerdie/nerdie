d435011425297c9be863bc3b60c870ec966fd516
Sean Coates <sean@seancoates.com>
2011-07-19 00:06:23 -0400

    allow passing of config file on command line

--- configulator.js
+++ configulator.js
@@ -1,11 +1,29 @@
 var fs = require('fs')
   , properties = {}
-  , configFile = process.cwd() + '/config.json';
+  , configFile
+  , configFileContents;
+
+if (process.argv[2]) {
+	try {
+		configFile = process.argv[2];
+		configFileContents = fs.readFileSync(configFile, 'utf8');
+	} catch (e) {
+		console.log("Can't load custom config file:", configFile);
+		process.exit(1);
+	}
+} else {
+	try {
+		configFile = process.cwd() + '/config.json';
+		configFileContents = fs.readFileSync(configFile);
+	} catch (e) {
+		console.log("Can't load default config file:", configFile);
+	}
+}
 
 try {
-	properties = JSON.parse(fs.readFileSync(configFile, 'utf8'));	
+	properties = JSON.parse(configFileContents);
 } catch (err){
-	console.log('Your configuration file contains a syntax error. Aborting.');
-	process.exit(0);
+	console.log('Your configuration file (%s) contains a syntax error. Aborting.', configFile);
+	process.exit(1);
 }
 module.exports = properties;
